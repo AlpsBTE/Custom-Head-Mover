f229a57ba19d3467d743f8181da33d717fee11f9
LordTuxn <r3tuxn@gmail.com>
2022-07-03 11:35:31 +0200

    remove world attribute from CustomHead

--- API/src/main/java/com/alpsbte/customheadmover/CustomHead.java
+++ API/src/main/java/com/alpsbte/customheadmover/CustomHead.java
@@ -5,14 +5,12 @@ import org.bukkit.block.BlockFace;
 
 public final class CustomHead {
     private final String texture;
-    private final String world;
     private final int x, y, z;
     private final String rotation;
     private final boolean isPlacedOnWall;
 
     public CustomHead(String texture, Location location, BlockFace rotation, boolean isPlacedOnWall) {
         this.texture = texture;
-        this.world = location.getWorld().getName();
         this.x = location.getBlockX();
         this.y = location.getBlockY();
         this.z = location.getBlockZ();
@@ -22,10 +20,6 @@ public final class CustomHead {
 
     public String getB64Texture() {
         return texture;
-    }
-
-    public String getWorld() {
-        return world;
     }
 
     public int getX() {
